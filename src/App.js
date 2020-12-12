3941e8eda13a44f49e479dc3049743e0d276b4f9
Steve <stevestephenas7@gmail.com>
2020-12-12 23:36:57 +0530

    Created header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
+
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM naming conversion
     <div className="app">
-      <Header />
+      <Router>
+        <div className="header">
+          <Link to="/">
+            <img
+              className="header__icon"
+              src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+              alt="Logo"
+            />
+          </Link>
+
+          <div className="header__center">
+            <input type="text" />
+            <SearchIcon />
+          </div>
+
+          <div className="header__right">
+            <p>Become a host</p>
+            <LanguageIcon />
+            <ExpandMoreIcon />
+            <Avatar />
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
