c51b2fe72bf9d2d484fc25535915b62bbdb0c44a
Steve <stevestephenas7@gmail.com>
2020-12-13 22:52:40 +0530

    Created footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      
+    <div className="footer">
+      <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
