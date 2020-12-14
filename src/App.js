bfd1bb000ebb1ba3f82b93f8c8137e8dc0497fff
Steve <stevestephenas7@gmail.com>
2020-12-14 22:03:59 +0530

    Final changes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             <img
               className="header__icon"
               src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-              alt="Logo"
+              alt="logo"
             />
           </Link>
 
